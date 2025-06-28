fb45491ff53e361e85bd7475c41a118252dadede
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 00:54:30 +0600

    Substring string method

---
+++ SubStringMethod.java
@@ -0,0 +1,15 @@
+
+/*
+Extracts a part of the String between start index and end index
+*/
+
+public class SubStringMethod {
+ public static void main(String[] args) {
+
+  String str = "Engineering";
+  System.out.println(str.substring(3,8));
+ }
+
+}
+
+//output: ineer
