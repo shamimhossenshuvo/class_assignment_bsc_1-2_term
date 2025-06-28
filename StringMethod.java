7159ec1f5b34d728f520f7fc9c959256c2b34a42
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 00:46:07 +0600

    contains() String method

---
+++ StringMethod.java
@@ -0,0 +1,8 @@
+public class StringMethod {
+ public static void main(String[] args) {
+
+  String str = "shuvo";
+  System.out.println(str);
+ }
+
+}
---
+++ containsString.java
@@ -0,0 +1,17 @@
+
+/*
+This method cheack whether a specific Substring is present inside the main String
+*/
+
+public class containsString {
+ public static void main(String[] args) {
+
+  String str = "Imperial collage of engineering";
+  System.out.println(str.contains("collage"));
+  System.out.println(str.contains("shuvo"));
+ }
+
+}
+
+//output: true
+//output: false
