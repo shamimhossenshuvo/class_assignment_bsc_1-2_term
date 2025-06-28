24f422f1b94997a13851b711e8c5f0e64e5ac94e
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 01:00:26 +0600

    Join() method of string

---
+++ joinString.java
@@ -0,0 +1,14 @@
+/*
+Extracts a part of the String between start index and end index
+*/
+
+public class joinString {
+ public static void main(String[] args) {
+
+  String result = String.join(" ","Imperial","Collage","Of","Engineering");
+  System.out.println(result);
+ }
+
+}
+
+//output: ineer
