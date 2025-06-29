b44b01de83e56d5a866cc10841324ddc9bf39881
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 23:10:09 +0600

    toCharArray()

---
+++ String Method/toCharArray.java
@@ -0,0 +1,15 @@
+/*
+Converts String into a character array
+*/
+
+import java.util.Arrays;
+public class toCharArray {
+ public static void main(String[] args) {
+
+  char[] arr = "java".toCharArray();
+   System.out.println(Arrays.toString(arr));
+ }
+
+}
+
+//Output: [j, a, v, a]
