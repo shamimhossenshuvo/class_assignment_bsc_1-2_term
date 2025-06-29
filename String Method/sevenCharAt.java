b63825da2b0380db90987fcda8aacf9e5206234e
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 18:10:08 +0600

    charAt()

---
+++ String Method/sevenCharAt.java
@@ -0,0 +1,14 @@
+/*
+Extracts Characters at a specific position in a string
+*/
+
+public class sevenCharAt{
+public static void main(String[] args) {
+
+        String s = "ICE";
+        System.out.println(s.charAt(1));
+}
+
+}
+
+//Output: C
