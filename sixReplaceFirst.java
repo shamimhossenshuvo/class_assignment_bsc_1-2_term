4358bbb3b4c05f6f12ad04440555011246c1cfa1
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 17:57:24 +0600

    String Method replace

---
+++ sixReplaceFirst.java
@@ -0,0 +1,12 @@
+/*
+Joins multiple strings using a separator (delimiter).
+*/
+
+public class sixReplaceFirst{
+    public static void main(String[] args) {
+          String s = "123-456-789";
+          System.out.println(s.replaceFirst("\\d+","***"));
+    }
+}
+
+//output: Imperial Collage Of Engineering
