b30ff102ad5da2b489b66949637d64c2efec6f01
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 23:53:28 +0600

    contentEquals()

---
+++ String Method/contentEquals.java
@@ -0,0 +1,14 @@
+/*
+checks if String and character sequence are equal
+*/
+
+
+public class contentEquals {
+ public static void main(String[] args) {
+
+   System.out.println("java".contentEquals(new StringBuilder("java")));
+ }
+
+}
+
+//Output:true
