84fa3d3e0f04d8464245768c87442f7727dd5180
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 22:55:56 +0600

    toUpperCase()

---
+++ String Method/toUpperCase.java
@@ -0,0 +1,14 @@
+/*
+convert String to all uppercase letters
+*/
+
+public class toUpperCase {
+ public static void main(String[] args) {
+
+   String s = "imperial";
+   System.out.println(s.toUpperCase());
+ }
+
+}
+
+//Output: IMPERIAL
