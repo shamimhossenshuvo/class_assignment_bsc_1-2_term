d32c3d49d9331330cff9a6c7a771f974b1c0a20e
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 23:37:11 +0600

    endsWith()

---
+++ String Method/endsWith.java
@@ -0,0 +1,15 @@
+/*
+check is the string Starts with a given word
+*/
+
+
+public class endsWith {
+ public static void main(String[] args) {
+
+  String s = "Imperial Collage of engineering";
+   System.out.println(s.endsWith("Imperial"));
+ }
+
+}
+
+//Output:false
