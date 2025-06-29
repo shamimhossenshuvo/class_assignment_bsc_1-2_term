4e9c0816c042d25e8e270138c8bc369669b942f3
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-30 00:02:26 +0600

    subSequence()

---
+++ String Method/subSequence.java
@@ -0,0 +1,16 @@
+/*
+similar to substring returns part of the String as a Sequence
+*/
+
+
+public class subSequence {
+ public static void main(String[] args) {
+
+    CharSequence cs = "Programming".subSequence(0, 6);
+    System.out.println(cs);
+
+ }
+
+}
+
+//Output:"Progra"
