5e9767f55d2d6d2dec1a88a53213fdf1bb654bcd
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 22:21:54 +0600

    compareTo()

---
+++ String Method/tenCompareTo.java
@@ -0,0 +1,17 @@
+/*
+compare to String alphabetically
+*/
+
+public class tenCompareTo {
+ public static void main(String[] args) {
+
+   String s1 = "Learn CSE";
+   String s2 = "Learn CE";
+   int result;
+   result = s1.compareTo(s2);
+   System.out.println(result);
+ }
+
+}
+
+//Output: 14
