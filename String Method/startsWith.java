9ca2c2a627c643e5ed00d3a5d960d7c1a2a379f3
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 23:30:47 +0600

    startsWith()

---
+++ String Method/startsWith.java
@@ -0,0 +1,15 @@
+/*
+Checks if entire String matches regex pattern
+*/
+
+
+public class startsWith {
+ public static void main(String[] args) {
+
+  String s = "Imperial Collage of engineering";
+   System.out.println(s.startsWith("Imperial"));
+ }
+
+}
+
+//Output:True
