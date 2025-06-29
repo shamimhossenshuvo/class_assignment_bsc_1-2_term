746b0330b04a22fd96f0c59679f9f9153134a496
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 22:53:14 +0600

    toLowerCase()

---
+++ String Method/toLowerCase.java
@@ -0,0 +1,14 @@
+/*
+convert String to all Lowercase letters
+*/
+
+public class toLowerCase {
+ public static void main(String[] args) {
+
+   String s = "IMPERIAL";
+   System.out.println(s.toLowerCase());
+ }
+
+}
+
+//Output: imperial
