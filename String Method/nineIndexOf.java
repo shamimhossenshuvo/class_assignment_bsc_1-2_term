89f8f31fc5a213f0ff32269c7114afda550e394b
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 18:31:59 +0600

    indexOf()

---
+++ String Method/nineIndexOf.java
@@ -0,0 +1,16 @@
+
+
+/*
+indicates where a character first occurs in a String
+*/
+
+public class nineIndexOf {
+ public static void main(String[] args) {
+
+   String s = "shuvo";
+   System.out.println(s.indexOf("u"));
+ }
+
+}
+
+//Output: 2
