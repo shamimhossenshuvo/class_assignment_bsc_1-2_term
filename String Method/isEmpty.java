1c8c861e540435c2e9d8f0c27045b7d3f1849e9e
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 23:41:36 +0600

    isEmpty()

---
+++ String Method/isEmpty.java
@@ -0,0 +1,15 @@
+/*
+check whether the string the has zero lenth
+*/
+
+
+public class isEmpty {
+ public static void main(String[] args) {
+
+  String s = "";
+   System.out.println(s.isEmpty());
+ }
+
+}
+
+//Output:True
