96fe65a1d8a62f026e6331fc29d53994b1b66486
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 23:57:01 +0600

    hasHCode()

---
+++ String Method/hashCode.java
@@ -0,0 +1,15 @@
+/*
+returns of the hash code for the string (used in hasing)
+*/
+
+
+public class hashCode {
+ public static void main(String[] args) {
+
+    String s = "Shuvo";
+   System.out.println(s.hashCode());
+ }
+
+}
+
+//Output:79866713
