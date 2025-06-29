2429af4279b956501cf0f556ad468e50cb9f3c06
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 17:48:53 +0600

    String Method replaceAll()

---
+++ fiveReplaceAll.java
@@ -0,0 +1,14 @@
+/*
+Joins multiple strings using a separator (delimiter).
+*/
+
+public class fiveReplaceAll {
+ public static void main(String[] args) {
+
+  String s = "A1B2C3";
+  System.out.println(s.replaceAll("\\d","*"));
+ }
+
+}
+
+//output: Imperial Collage Of Engineering
