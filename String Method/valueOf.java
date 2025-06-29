e4f7af0e65179e55c958a5625045d4e83b8c0435
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 23:02:23 +0600

    valueOf()

---
+++ String Method/valueOf.java
@@ -0,0 +1,15 @@
+/*
+converts other types (int, float, char, boolean)
+*/
+
+public class valueOf {
+ public static void main(String[] args) {
+
+   int x = 50;
+   String s = String.valueOf(x);
+   System.out.println(s);
+ }
+
+}
+
+//Output: "50"
