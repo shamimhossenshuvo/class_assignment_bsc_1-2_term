9b4820b317d872882de116291dc4420e8fdab6d7
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-06-29 17:43:30 +0600

    String Method replace()

---
+++ fourReplaceString.java
@@ -0,0 +1,14 @@
+/*
+Find a character and replace it with another character
+*/
+
+public class fourReplaceString {
+ public static void main(String[] args) {
+
+  String s = "Apple";
+  System.out.println(s.replace('p','b'));
+ }
+
+}
+
+//output: Abble
