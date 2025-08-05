8ce510286e88007cfdd2a3bd98bade78c3fdb245
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-08-05 19:05:43 +0600

    new problem submit (ABit)

--- codeforces/ABit.java
+++ codeforces/ABit.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class ABit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = 0;
+        int n = input.nextInt();
+
+        while (n-- > 0) {
+            String x = input.next();
+            if (x.contains("++")) {
+                a++;
+            } else {
+                a--;
+            }
+        }
+        System.out.println(a);
+    }
 }
