ca2ed3fceb168fdeb6c440782206e4767b3b13dd
shamimhossenshuvo <shamimfarazi51@gmail.com>
2025-08-09 18:38:24 +0600

    Sum All Vlaues Array

---
+++ Array problem from MI/SumAllVlauesArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class SumAllVlauesArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a[] = new int[7];
+
+        int n = a.length;
+
+        System.out.println("Enter your array numbers: ");
+
+        for (int i = 0; i < n; i++){
+            a[i] = input.nextInt();
+
+        }
+        System.out.println("List of array Numbers " + Arrays.toString(a));
+        int i = 0;
+        int sum = a[i] + a[i+1];
+        for(i = 2; i < n ; i++) {
+            sum = sum + a[i];
+
+        }
+        System.out.println(sum);
+    }
+
+}
